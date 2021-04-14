4023fc1619607597a8e9dc77e0f16f51449b72b9
Miguel Murillo Gonzalez <mmurillo@cic.es>
2021-04-14 12:59:14 +0200

    modified preprocessing.py

--- Lib/preprocessing.py
+++ Lib/preprocessing.py
@@ -34,6 +34,8 @@ Raises
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.base import TransformerMixin
+from sklearn.preprocessing import OneHotEncoder
 import scipy
 import pywt
 
@@ -404,3 +406,396 @@ def min_feature(data, rolling_window, num):
         return np.min(data[-(rolling_window-1+num):-(num-1)])
 
 
+def quantile_feature(data, rolling_window, num):
+    """Computes the median value for a given lag and its rolling window"""
+
+    """
+    Given a set number of lags in the past, a set rolling window and the input 
+    dataset, computes the rolling median value for the given window.
+
+    Parameters
+    ----------
+    data: pd.Dataframe
+        input dataframe containing the time series.
+    num: int
+        lag number to calculate.
+    rolling_window: list of int
+        rolling window over which to calculate the feature.
+    
+    Returns
+    -------
+    float
+        calculated feature
+    """   
+    if num == 1:
+        return np.quantile(data[-rolling_window:], 0.5)
+    else:
+        return np.quantile(data[-(rolling_window-1+num):-(num-1)], 0.5)
+
+
+def iqr_feature(data, rolling_window, num):
+    """Computes the interquartile range for a given lag and its rolling 
+    window"""
+
+    """
+    Given a set number of lags in the past, a set rolling window and the input 
+    dataset, computes the rolling interquartile range value for the given 
+    window.
+
+    Parameters
+    ----------
+    data: pd.Dataframe
+        input dataframe containing the time series.
+    num: int
+        lag number to calculate.
+    rolling_window: list of int
+        rolling window over which to calculate the feature.
+    
+    Returns
+    -------
+    float
+        calculated feature
+    """ 
+    if num == 1:
+        return scipy.stats.iqr(data[-rolling_window:], rng = [25, 75])
+    else:
+        return scipy.stats.iqr(data[-(rolling_window-1+num):-(num-1)], 
+                               rng = [25, 75])
+    
+
+#-------------------------------------------------------------------------------
+
+class Lags(TransformerMixin):
+    """Class containing methods to create the lagged time series"""
+
+    """
+    A class that defines a transformer to create the lagged time series. Based 
+    on the TransformerMixin class from the scikit-learn library. so it supports
+    the fit, transform and fit_transform arguments.
+
+    Parameters
+    ----------
+    lag: int {2, other}
+        Number of lags to be computed for each step in the time series.
+    step: int {1, other}
+        Number of steps in between each lag, expressed in the frequency of the
+        time series.
+    dropna: bool {True, False}
+        Defines wether to include or not the initial NA values in the lagged
+        time series. True by default. Will cause errors if set to False.
+    
+    Returns
+    -------
+    pd.Dataframe
+        Containing the initial time series with the lagged features.
+    """
+    def __init__(self, lag=2, step=1, dropna=True):
+        self.lag = lag
+        self.dropna = dropna
+        self.step = step
+        pass
+    def fit(self, X, y=None): 
+        return self
+    def transform(self, X, y=None):
+        X = X.iloc[:,0]                                                         
+        if type(X) is pd.DataFrame:                                             
+            new_dict={}                                                        
+            for col_name in X:                                                 
+                for l in range(1,self.lag+1, self.step):                    
+                    new_dict['%s_lag%d' %(col_name,l)]=X[col_name].shift(l) 
+            res=pd.DataFrame(new_dict,index=X.index)
+
+        elif type(X) is pd.Series:
+            the_range=range(0,self.lag+1, self.step)
+            res=pd.concat([X.shift(i) for i in the_range],axis=1)
+            res.columns=['lag_%d' %i for i in the_range]
+        else:
+            raise TypeError('Input data must be in the form of Pandas series' +
+            'or dataframe but was %d'.format(type(X)))   
+            print('Only works for DataFrame or Series')
+            return None
+        if self.dropna:
+            res = res.dropna()
+            res = res[res.columns[::-1]] 
+            return res
+        else:
+            res = res[res.columns[::-1]]
+            return res
+
+def lags_sample(df, window_length = 5, step = 1):
+    X = Lags(lag = window_length, step=step).transform(df) 
+    X = X.iloc[:, :-1]
+    return X   
+
+
+def minute_sample(X):
+    """OneHot encoding for the minute in hour feature"""
+
+    enc = OneHotEncoder(sparse='False', drop='first',
+                        categories=[list(range(0,61))])
+    array = enc.fit_transform(np.array(X.index.minute).reshape(-1,1)).toarray()
+    columns = ['minute_'+str(i) for i in range(1,61)]
+    return pd.DataFrame(array, index = X.index, columns = columns)
+
+
+def hour_sample(X):
+    """Onehot encoding for the hour of day feature"""
+    enc = OneHotEncoder(sparse='False', drop='first', 
+                        categories=[list(range(0,25))])
+    array = enc.fit_transform(np.array(X.index.hour).reshape(-1,1)).toarray()
+    columns = ['hour_'+str(i) for i in range(1,25)]
+    return pd.DataFrame(array, index = X.index, columns = columns)
+
+
+def dayofweek_sample(X):
+    """OneHot encoding for the day of week feature"""
+    enc = OneHotEncoder(sparse='False', drop='first', 
+                        categories=[list(range(0,8))])
+    array = enc.fit_transform(np.array(X.index.dayofweek).reshape(-1,1))
+    array = array.toarray()
+    columns = ['dayofweek_'+str(i) for i in range(1,8)]
+    return pd.DataFrame(array, index = X.index, columns = columns)
+
+def day_sample(X):
+    """OneHot encoding for the day of month feature"""
+    enc = OneHotEncoder(sparse='False', drop='first',
+                        categories=[list(range(0,32))])
+    array = enc.fit_transform(np.array(X.index.day).reshape(-1,1)).toarray()
+    columns = ['day_'+str(i) for i in range(1,32)]
+    return pd.DataFrame(array, index = X.index, columns = columns)
+
+
+def month_sample(X):
+    """OneHot encoding for month of year feature"""
+    enc = OneHotEncoder(sparse='False', drop='first', 
+                        categories=[list(range(0,13))])
+    array = enc.fit_transform(np.array(X.index.month).reshape(-1,1)).toarray()
+    columns = ['month_'+str(i) for i in range(1,13)]
+    return pd.DataFrame(array, index = X.index, columns = columns)
+
+
+def quarter_sample(X):
+    """OneHot encoding for quarter of year feature"""
+    enc = OneHotEncoder(sparse='False', drop='first', 
+                        categories=[list(range(0,5))])
+    array = enc.fit_transform(np.array(X.index.quarter).reshape(-1,1)).toarray()
+    columns = ['quarter_'+str(i) for i in range(1,5)]
+    return pd.DataFrame(array, index = X.index, columns = columns)
+
+def weekofyear_sample(X):
+    """OneHot encoding for week number feature"""
+    enc = OneHotEncoder(sparse='False', drop='first', 
+                        categories=[list(range(0,53))])
+    array = enc.fit_transform(np.array(X.index.isocalendar().week).reshape(-1,1))
+    array = array.toarray()
+    columns = ['weekofyear_'+str(i) for i in range(1,53)]
+    return pd.DataFrame(array, index = X.index, columns = columns)
+
+
+def weekend_sample(X):
+    """OneHot encoding for weekend feature (Binary)"""
+    saturday = X.index.dayofweek == 5
+    sunday = X.index.dayofweek == 6
+    enc = OneHotEncoder(sparse='False', drop='first', 
+                        categories=[list(range(0,2))])
+    array = enc.fit_transform(np.logical_or(saturday, sunday).reshape(-1,1))
+    array = array.toarray()
+    columns = ['weekend_'+str(i) for i in range(1,2)]
+    return pd.DataFrame(array, index = X.index, columns = columns)
+
+
+def mean_sample(X, rolling_window = 2):
+    X_rol_mean = pd.DataFrame(X.iloc[:,:].rolling(rolling_window).mean()) 
+    X_rol_mean.rename(columns=lambda x: x.replace('lag', 'mean'+'_'+
+                      str(rolling_window)), 
+                      inplace=True)
+    return X_rol_mean
+
+
+def std_sample(X, rolling_window = 2):
+    X_rol_std = pd.DataFrame(X.iloc[:,:].rolling(rolling_window).std()) 
+    X_rol_std.rename(columns=lambda x: x.replace('lag', 'std'+'_'+
+                     str(rolling_window)), 
+                     inplace=True)
+    return X_rol_std
+
+
+def min_sample(X, rolling_window = 2):
+    X_rol_min = pd.DataFrame(X.iloc[:,:].rolling(rolling_window).min())
+    X_rol_min.rename(columns=lambda x: x.replace('lag', 'min'+'_'+
+                     str(rolling_window)), 
+                     inplace=True)
+    return X_rol_min
+
+
+def max_sample(X, rolling_window = 2):
+    X_rol_max = pd.DataFrame(X.iloc[:,:].rolling(rolling_window).max())
+    X_rol_max.rename(columns=lambda x: x.replace('lag', 'max'+'_'+
+                     str(rolling_window)), 
+                     inplace=True)
+    return X_rol_max
+
+
+def quantile_sample(X, rolling_window = 2):
+    X_q = pd.DataFrame(X.iloc[:,:].rolling(rolling_window).quantile(0.5))
+    X_q.rename(columns=lambda x: x.replace('lag', 'quantile'+'_'+
+               str(rolling_window)), 
+               inplace=True)
+    return X_q
+
+
+def iqr_sample(series, rolling_window = 2):
+    r_in=[]
+    for i in list(range(len(series)-rolling_window+1)):
+        r_in.append(scipy.stats.iqr(series.iloc[i:rolling_window+i,:], axis=0))
+    r_in = pd.DataFrame(r_in)
+    r_in.index = series.iloc[rolling_window-1:,:].index
+    r_in.columns = series.columns
+    r_in.rename(columns=lambda x: x.replace('lag', 'iqr'+'_'+
+                str(rolling_window)), 
+                inplace=True)
+    return r_in
+
+#-------------------------------------------------------------------------------
+
+def switch_sample_features(value, X, lags_X, rolling_window):
+    """Switch for selecting sample features to add to the initial time series"""
+
+    """
+    A switch, that takes as input the 
+    
+    Parameters
+    ----------
+    value: list of str
+
+    X: pd.Dataframe
+
+    lags_X: list of int
+
+    rolling_window: list of int
+
+
+    Returns
+    -------
+    function call
+        Function call to corresponding sample feature creation 
+    """
+    return { 
+        'mean': lambda : mean_sample(lags_X, rolling_window),
+        'std': lambda : std_sample(lags_X, rolling_window),
+        'max': lambda : max_sample(lags_X, rolling_window),
+        'min': lambda : min_sample(lags_X, rolling_window),
+        'quantile': lambda : quantile_sample(lags_X, rolling_window),
+        'iqr': lambda : iqr_sample(lags_X, rolling_window),
+        #-----------------------------------------
+        'minute': lambda : minute_sample(X),
+        'hour': lambda : hour_sample(X),
+        'dayofweek': lambda : dayofweek_sample(X),
+        'day': lambda : day_sample(X),
+        'month': lambda : month_sample(X),
+        'quarter': lambda : quarter_sample(X),
+        'weekofyear': lambda : weekofyear_sample(X),
+        'weekend': lambda : weekend_sample(X)
+        
+    }.get(value)()
+
+
+def switch_features(value, lags_data, date, rolling_window, num):
+    """Switch for selecting features to add to the initial time series"""
+
+    """
+    A switch, that takes as input the 
+    
+    Parameters
+    ----------
+    value: list of str
+
+    X: pd.Dataframe
+
+    lags_data: list of int
+
+    date: datetime
+        
+    rolling_window: list of int
+
+    num: int
+
+
+    Returns
+    -------
+    function call
+        Function call to corresponding feature creation 
+    """
+    return {
+        'lag': lambda : lag_feature(lags_data, num),
+        #--------------------------------------------------------------
+        'mean': lambda : mean_feature(lags_data, rolling_window, num),
+        'std': lambda : std_feature(lags_data, rolling_window, num),
+        'max': lambda : max_feature(lags_data, rolling_window, num),
+        'min': lambda : min_feature(lags_data, rolling_window, num),
+        'quantile': lambda : quantile_feature(lags_data, rolling_window, num),
+        'iqr': lambda : iqr_feature(lags_data, rolling_window, num),
+        #--------------------------------------------------------------
+        'minute': lambda : minute_feature(date, num),
+        'hour': lambda : hour_feature(date, num),
+        'dayofweek': lambda : dayofweek_feature(date, num),
+        'day': lambda : day_feature(date, num),
+        'month': lambda : month_feature(date, num),
+        'quarter': lambda : quarter_feature(date, num),
+        'weekofyear': lambda : weekofyear_feature(date, num),
+        'weekend': lambda : weekend_feature(date, num)
+    }.get(value)()
+
+
+def switch_optimization(value, regressor):
+    return {
+        'tpe': lambda : tpe_optimization(),
+        'pso': lambda : pso_optimization(),
+    }.get(value)()
+
+
+#-------------------------------------------------------------------------------
+
+def recursive_forecast(y, model, window_length, feature_names, 
+                       rolling_window, n_steps):
+    """Recursive forecasting workflow for a pre-trained model"""
+
+    """
+    Recursive forecasting workflow. Works by dinamically modifying the lags in
+    the dataset, replacing the most recently predicted value for a feature set
+    instead of the most recent lag, so at the next iteration values are computed
+    with the previous iteration's data
+
+    Parameters
+    ----------
+    y: pd.Series 
+        holding the input time-series to forecast
+    model: pre-trained machine learning model
+    lags: list of int
+        list of lags used for training the model
+    n_steps: int
+        number of time periods in the forecasting horizon
+    step: int
+        forecasting time period
+   
+    Returns
+    -------
+    fcast_values: pd.Series with forecasted values
+    """
+   
+    last_date = y.index[-1] + datetime.timedelta(minutes=15) 
+    target_range = pd.date_range(last_date, periods=n_steps, freq=freq)
+    target_value = np.arange(n_steps, dtype = float)
+    max_rol = max(rolling_window, default=1)
+    lags = list(y.iloc[-(window_length+(max_rol-1)):,0].values)
+    ####
+    
+    
+    for i in range(n_steps):
+        train = create_features(feature_names, lags, target_range[i]) 
+        new_value = model.predict(pd.DataFrame(train).transpose())
+        target_value[i] = new_value[0]
+        lags.pop(0) 
+        lags.append(new_value[0]) 
+
+    return target_value
