6bd19471c6f67ed6d90abe21dc11d8e4b69d3c4e
Miguel Murillo <mmurillo@cic.es>
2021-04-30 11:48:01 +0200

    Added docstrings to metrics.py

---
+++ ts_automl/metrics.py
@@ -0,0 +1,13 @@
+"""
+Module containing metrics to evaluate the performance of the different models
+
+This module contains metric with which to evaluate the performance of the time
+series prediction pipelines in the library. It contains both absolute metrics, 
+such as mse or mape, as well as a measure of error with respect to a naïve 
+model.
+"""
+
+def mse(y_pred, y_test):
+    """
+    rmse ca
+    """
