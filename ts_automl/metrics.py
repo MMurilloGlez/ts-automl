3b42f8e415c7885276c8f1b174e881864bb116c0
Miguel Murillo <mmurillo@cic.es>
2021-05-03 08:57:16 +0200

    added

--- ts_automl/metrics.py
+++ ts_automl/metrics.py
@@ -10,6 +10,9 @@ model.
 import numpy as np
 import pandas as pd
 from sklearn import metrics
+from sktime.forecasting.naive import NaiveForecaster
+
+#--Traditional------------------------------------------------------------------
 
 def mse(y_true, y_pred):
     """mean square error calculation.
@@ -29,28 +32,35 @@ def mse(y_true, y_pred):
     float
         value of the error metric
     """
+    error = metrics.mean_squared_error(y_true, y_pred, squared= True)
 
     return error
 
 def rmse(y_true, y_pred):
     """root mean square error calculation"""
+    error = metrics.mean_squared_error(y_true, y_pred, squared= False)
 
     return error
 
 def rsquare(y_true, y_pred):
     """R-Square error calculation"""
+    error = metrics.r2_score(y_true, y_pred)
 
     return error
 
 def mape(y_true, y_pred):
     """Mean absolute percentage error calculation"""
+    error = metrics.mean_absolute_percentage_error(y_true, y_pred)
 
     return error
 
-def smape(y_pred, y_test):
-    """Symmetrical mean absolute percentage error calculation"""
-
+def exp_var(y_true, y_pred):
+    """Mean absolute percentage error calculation"""
+    error = metrics.explained_variance_score(y_true, y_pred)
+    
     return error
+
+#--With respect to naive-----------------------------------------------------------------------------
 
 def relative_error(y_pred, y_test):
     """Relative error improvement with respect to a naïve prediction model
@@ -60,5 +70,5 @@ def relative_error(y_pred, y_test):
     float
         Percentage of improvement with respect to naive model
     """
-
+    naive = NaiveForecaster(strategy='mean', window_length=window)
     return error
