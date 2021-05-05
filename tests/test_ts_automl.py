97c65b4981a8362d42d054e67557c6c8087384e9
Miguel Murillo <mmurillo@cic.es>
2021-05-05 11:45:06 +0200

    started writing tests

--- tests/test_ts_automl.py
+++ tests/test_ts_automl.py
@@ -2,4 +2,23 @@ from ts_automl import __version__
 
 
 def test_version():
-    assert __version__ == '0.1.0'
+    assert __version__ == '0.1.1'
+
+
+def slow_prediction_test():
+
+    rel_error = 0
+    assert rel_error < 100
+
+
+def bal_prediction_test():
+
+    rel_error = 0
+    assert rel_error < 100
+
+
+def fast_prediction_test():
+
+    rel_error = 0
+    assert rel_error < 100
+
