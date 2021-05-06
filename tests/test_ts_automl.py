dd692fe189989148ac667a2d66ba22b5706a0465
Miguel Murillo <mmurillo@cic.es>
2021-05-06 12:28:50 +0200

    added test functions using pytest

--- tests/test_ts_automl.py
+++ tests/test_ts_automl.py
@@ -1,5 +1,211 @@
 from ts_automl import __version__
+from ts_automl.pipelines import fast_prediction
+from ts_automl.pipelines import balanced_prediction
+from ts_automl.pipelines import slow_prediction
 
 
+def test_fast_1():
+    """Test for fast prediction time, using knn, series 1"""
+    p, e, r = fast_prediction(filename='./tests/test_series/Serie1.csv',
+                              freq='10T',
+                              targetcol='VALUE',
+                              datecol='DATE',
+                              sep=';',
+                              decimal=',',
+                              date_format="%d/%m/%Y %H:%M:%S.%f",
+                              plot=False)
+
+    assert r < 100
+
+
+def test_fast_2():
+    """Test for fast prediction time, using knn, series 2"""
+    p, e, r = fast_prediction(filename='./tests/test_series/Serie2.csv',
+                              freq='1T',
+                              targetcol='VALUE',
+                              datecol='DATE',
+                              sep=';',
+                              decimal=',',
+                              date_format="%d/%m/%Y %H:%M:%S.%f",
+                              plot=False)
+
+    assert r < 100
+
+
+def test_fast_3():
+    """Test for fast prediction time, using knn, series 3"""
+    p, e, r = fast_prediction(filename='./tests/test_series/serie3.csv',
+                              freq='15T',
+                              targetcol='INSTALACIONES [kWh]',
+                              datecol='MSJO_DATUM',
+                              sep=';',
+                              decimal='.',
+                              date_format="%d/%m/%Y %H:%M",
+                              plot=False)
+
+    assert r < 100
+
+
+def test_fast_4():
+    """Test for fast prediction time, using knn, series 4"""
+    p, e, r = fast_prediction(filename='./tests/test_series/Serie4.csv',
+                              freq='5T',
+                              targetcol='VALUE',
+                              datecol='DATE',
+                              sep=';',
+                              decimal=',',
+                              date_format="%d/%m/%Y %H:%M:%S.%f",
+                              plot=False)
+
+    assert r < 100
+
+
+def test_fast_5():
+    """Test for fast prediction time, using knn, series 5"""
+    p, e, r = fast_prediction(filename='./tests/test_series/Serie5.csv',
+                              freq='20T',
+                              targetcol='VALUE',
+                              datecol='DATE',
+                              sep=';',
+                              decimal=',',
+                              date_format="%d/%m/%Y %H:%M:%S.%f",
+                              plot=False)
+
+
+def test_bal_1():
+    """Test for balanced prediction time, using lightgbm, series 1"""
+    p, e, r = balanced_prediction(filename='./tests/test_series/Serie1.csv',
+                                  freq='10T',
+                                  targetcol='VALUE',
+                                  datecol='DATE',
+                                  sep=';',
+                                  decimal=',',
+                                  date_format="%d/%m/%Y %H:%M:%S.%f",
+                                  plot=False)
+    assert r < 100
+
+
+def test_bal_2():
+    """Test for balanced prediction time, using lightgbm, series 1"""
+    p, e, r = balanced_prediction(filename='./tests/test_series/Serie2.csv',
+                                  freq='5T',
+                                  targetcol='VALUE',
+                                  datecol='DATE',
+                                  sep=';',
+                                  decimal=',',
+                                  date_format="%d/%m/%Y %H:%M:%S.%f",
+                                  plot=False)
+
+    assert r < 100
+
+
+def test_bal_3():
+    """Test for balanced prediction time, using lightgbm, series 3"""
+    p, e, r = balanced_prediction(filename='./tests/test_series/Serie3.csv',
+                                  freq='15T',
+                                  targetcol='INSTALACIONES [kWh]',
+                                  datecol='MSJO_DATUM',
+                                  sep=';',
+                                  decimal='.',
+                                  date_format="%d/%m/%Y %H:%M",
+                                  plot=False)
+
+    assert r < 100
+
+
+def test_bal_4():
+    """Test for balanced prediction time, using lightgbm, series 4"""
+    p, e, r = balanced_prediction(filename='./tests/test_series/Serie4.csv',
+                                  freq='5T',
+                                  targetcol='VALUE',
+                                  datecol='DATE',
+                                  sep=';',
+                                  decimal=',',
+                                  date_format="%d/%m/%Y %H:%M:%S.%f",
+                                  plot=False)
+
+    assert r < 100
+
+
+def test_bal_5():
+    """Test for balanced prediction time, using lightgbm, series 5"""
+    p, e, r = balanced_prediction(filename='./tests/test_series/Serie5.csv',
+                                  freq='20T',
+                                  targetcol='VALUE',
+                                  datecol='DATE',
+                                  sep=';',
+                                  decimal=',',
+                                  date_format="%d/%m/%Y %H:%M:%S.%f",
+                                  plot=False)
+
+    assert r < 100
+
+
+def test_slow_1():
+    """Test for slow prediction time, using keras, series 1"""
+    p, e, r = slow_prediction(filename='./tests/test_series/Serie1.csv',
+                              freq='10T',
+                              targetcol='VALUE',
+                              datecol='DATE',
+                              sep=';',
+                              decimal=',',
+                              date_format="%d/%m/%Y %H:%M:%S.%f",
+                              plot=False)
+
+    assert r < 100
+
+
+def test_slow_2():
+    """Test for slow prediction time, using keras, series 2"""
+    p, e, r = slow_prediction(filename='./tests/test_series/Serie2.csv',
+                              freq='1T',
+                              targetcol='VALUE',
+                              datecol='DATE',
+                              sep=';',
+                              decimal=',',
+                              date_format="%d/%m/%Y %H:%M:%S.%f",
+                              plot=False)
+
+    assert r < 100
+
+
+def test_slow_3():
+    """Test for slow prediction time, using keras, series 3"""
+    p, e, r = slow_prediction(filename='./tests/test_series/serie3.csv',
+                              freq='15T',
+                              targetcol='INSTALACIONES [kWh]',
+                              datecol='MSJO_DATUM',
+                              sep=';',
+                              decimal='.',
+                              date_format="%d/%m/%Y %H:%M",
+                              plot=False)
+
+    assert r < 100
+
+
+def test_slow_4():
+    """Test for fast prediction time, using keras, series 4"""
+    p, e, r = slow_prediction(filename='./tests/test_series/Serie4.csv',
+                              freq='5T',
+                              targetcol='VALUE',
+                              datecol='DATE',
+                              sep=';',
+                              decimal=',',
+                              date_format="%d/%m/%Y %H:%M:%S.%f",
+                              plot=False)
+
+    assert r < 100
+
+
+def test_slow_5():
+    """Test for slow prediction time, using keras, series 5"""
+    p, e, r = slow_prediction(filename='./tests/test_series/Serie5.csv',
+                              freq='20T',
+                              targetcol='VALUE',
+                              datecol='DATE',
+                              sep=';',
+                              decimal=',',
+                              date_format="%d/%m/%Y %H:%M:%S.%f",
+                              plot=False)
 def test_version():
     assert __version__ == '0.1.0'
